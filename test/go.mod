166c19fb4e018a4a236d1ce123e405ed75f62162
Alex Lewis <alexlewisnc@gmail.com>
2021-11-06 21:35:41 -0400

    Use published subdirectory in test

---
+++ test/go.mod
@@ -0,0 +1,5 @@
+module example.com
+
+go 1.16
+
+require github.com/alexgiesting/go-mod-test-sub/sub v0.1.1
---
+++ test/main.go
@@ -0,0 +1,7 @@
+package main
+
+import sub "github.com/alexgiesting/go-mod-test-sub/sub"
+
+func main() {
+	sub.Test()
+}
